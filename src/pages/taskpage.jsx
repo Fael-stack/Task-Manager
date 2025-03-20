1306eb30388012aa9a8194be957ca778e95daa06
Dev Aze <azevchk@gmail.com>
2025-03-20 15:53:27 -0300

    TaskPage Paramms

--- src/pages/taskpage.jsx
+++ src/pages/taskpage.jsx
@@ -1,5 +1,15 @@
-function taskPage() {
-  return <h1>Page</h1>;
+import { useSearchParams } from "react-router-dom";
+
+function TaskPage() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <h1>{title} </h1>
+      <p>{description}</p>
+    </div>
+  );
 }
 
-export default taskPage;
+export default TaskPage;
